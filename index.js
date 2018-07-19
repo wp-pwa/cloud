31966d30b8ee24b9e48a41c7d36fa3544f5f764e
Luis Herranz <luisherranz@gmail.com>
2018-07-19 11:31:22 +0200

    feat(got): install got. first version working

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.end('Welcome to Micro');
+/* eslint-disable no-underscore-dangle */
+const { parse } = require('url');
+const got = require('got');
+
+module.exports = async req => {
+  const url = parse(req.url.replace('/', ''));
+  if (/favicon/.test(url.href)) return '';
+  const response = await got(url, {
+    headers: {
+      'user-agent': req.headers['user-agent'],
+      host: url.host,
+    },
+  });
+  console.log(response.req._header);
+  return response.body;
 };
