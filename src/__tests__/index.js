23ff5b4b0ce9c1f6f2c5d538313ded9297d52ed5
Luis Herranz <luisherranz@gmail.com>
2018-07-19 16:58:36 +0200

    test(index): add first tests using test-listen and nock

---
+++ src/__tests__/index.js
@@ -0,0 +1,44 @@
+const micro = require('micro');
+const nock = require('nock');
+const listen = require('test-listen');
+const got = require('got');
+const server = require('../');
+
+let url = null;
+let service = null;
+
+beforeEach(async () => {
+  service = micro(server);
+  url = await listen(service);
+});
+
+afterEach(() => {
+  service.close();
+});
+
+test('Should throw if no url is passed', async () => {
+  await expect(got(`${url}/`)).rejects.toThrow();
+});
+
+test('Should throw if invalid url is passed', async () => {
+  await expect(got(`${url}/no-valid-url`)).rejects.toThrow();
+});
+
+test('Should return 500 status if it fails', async () => {
+  try {
+    await got(`${url}/no-valid-url`);
+  } catch (error) {
+    expect(error.statusCode).toBe(500);
+  }
+});
+
+test('Should return 404 status if API returns 404', async () => {
+  nock('http://fake-domain.com')
+    .get('/')
+    .reply(404, 'Not found!');
+  try {
+    await got(`${url}/http://fake-domain.com/`);
+  } catch (error) {
+    expect(error.statusCode).toBe(404);
+  }
+});
