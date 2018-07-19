cc715fd6eeb40ab9b44f0f963f74960b794e8d0d
Luis Herranz <luisherranz@gmail.com>
2018-07-19 16:13:01 +0200

    feat(index): add basic error handling

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const { parse } = require('url');
 const got = require('got');
 const cors = require('micro-cors')();
+const { send } = require('micro');
 
 module.exports = cors(async (req, res) => {
-  const url = parse(req.url.replace('/', ''));
-  const response = await got(url, {
-    headers: {
-      'User-Agent': req.headers['user-agent'],
-      Host: url.host,
-    },
-  });
-  res.setHeader(
-    'Cache-Control',
-    'public, max-age=0, s-maxage=180, stale-while-revalidate=31536000, stale-if-error=31536000',
-  );
-  return response.body;
+  try {
+    const url = parse(req.url.replace('/', ''));
+    const data = await got(url, {
+      headers: {
+        'User-Agent': req.headers['user-agent'],
+        Host: url.host,
+      },
+    });
+    res.setHeader(
+      'Cache-Control',
+      'public, max-age=0, s-maxage=180, stale-while-revalidate=31536000, stale-if-error=31536000',
+    );
+    return send(res, 200, JSON.parse(data.body));
+  } catch (error) {
+    return send(res, 400, error);
+  }
 });
